f2af73c960369aa702c5a5933c072cfb23e33bae
Alex <66781621+AlexTowny@users.noreply.github.com>
2022-09-18 18:48:35 +0400

    Create 4-9.cs

---
+++ 5st-practice/4-9.cs
@@ -0,0 +1,49 @@
+
+using System;
+class HelloWorld {
+ 
+  
+  static void R(int N, int D=2){
+        
+        
+        if( N%D == 0 ){
+            Console.Write("{0} ", D);
+            R(N/D, D);
+        }
+        else if(N!=1){
+            R(N, D+1);
+        }
+        
+        
+  }
+    
+  static void Main() {
+    int A = int.Parse(Console.ReadLine());
+    int B = int.Parse(Console.ReadLine());
+    
+    for(int i = A; i<=B; i++){
+        R(i);
+        Console.Write("\n");
+    }
+  }
+}
+/*
+2
+10
+2 
+3 
+2 2 
+5 
+2 3 
+7 
+2 2 2 
+3 3 
+2 5
+-----
+5
+5
+5
+-----
+
+
+*/
